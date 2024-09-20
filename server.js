31b7bab506d3f80b6961ac31958e381e3fb8da87
jorted-runner <jorted.runner1996@gmail.com>
2024-09-20 05:45:58 -0500

    Update to server... stripped it back

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./database/connect');
-const professionalRoutes = require('./routes/professional');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/professional', professionalRoutes);
-
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello');
 });
+
+app.listen(port);
+console.log('Web Server is listening at port ' + port);
